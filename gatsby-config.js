096f09509e8afdf9737585a746f16caffcfbfd74
BrenBln <brenbln94@gmail.com>
2017-11-30 03:12:56 -0800

    Placeholder set

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Application Development Compendium`,
+    title: `Application Development`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `src`,
+          path: `${__dirname}/src/`,
+        },
+    },
+`gatsby-transformer-remark`,        
+  `gatsby-plugin-glamor`,
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography`,
+    },
+  },
+],
   pathPrefix: `/appdevhelp`
 }
