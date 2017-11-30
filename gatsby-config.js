0aac23bcb10a6c976eb540500ec4b7290783d1fd
BrenBln <brenbln94@gmail.com>
2017-11-29 22:31:05 -0800

    Package and configuration updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Application Development Compendium`,
   },
   plugins: [`gatsby-plugin-react-helmet`],
+  pathPrefix: '/appdevhelp'
 }
