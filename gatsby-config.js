5d410e55778aac289d240c8518d497c77f3dca42
Bren <b_belen@u.pacific.edu>
2017-12-13 20:16:22 -0800

    Added plugin for netlify redirect support

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
 `gatsby-transformer-remark`,
   `gatsby-plugin-glamor`,
+  `gatsby-plugin-netlify`,
   `gatsby-plugin-jss`,
   {
     resolve: `gatsby-plugin-typography`,
